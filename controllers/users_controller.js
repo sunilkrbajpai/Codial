cd9b9a15abb92cd5430a67e9db3b6528285648af
sunil <sunilkumarbajpai.skb@gmail.com>
2020-06-11 14:11:10 +0530

    added sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,5 +3,16 @@ module.exports.profile=function(req,res){
     {
         title:"Users Profile"
     });
-
+}
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',
+    {
+        title:"sign up"
+    });
+}
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',
+    {
+        title:"sign in"
+    });
 }
