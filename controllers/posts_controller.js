b2afcf43d7f9ea549e0bb07fc616ef52f39a8ffe
sunil <sunilkumarbajpai.skb@gmail.com>
2020-06-14 01:01:17 +0530

    delete comments and also posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post=require('../models/posts');
+const Comment=require('../models/comment');
+
 
 module.exports.create=function(req,res)
 {
@@ -11,4 +13,21 @@ module.exports.create=function(req,res)
         return res.redirect('back')
     }
     )
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //.id means converting the object id into string
+        if(post.user==req.user.id)
+        {
+            post.remove();
+            Comment.deleteMany({post:req.params.id},function(err)
+            {
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,5 @@ const passport=require('passport');
 const postsCOntroller=require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication,postsCOntroller.create);
+router.get('/destroy/:id',passport.checkAuthentication,postsCOntroller.destroy);
 module.exports=router;
